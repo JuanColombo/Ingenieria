865b59a1d51fb279bcabe10b68c1e1aa37b1501c
JuanColombo <juampi.colombo@hotmail.com>
2019-10-07 22:47:25 -0300

    Incorporacion de menues hamburguesa y jquery

--- js/codigo.js
+++ js/codigo.js
@@ -3,8 +3,19 @@ $(document).ready(
     {
               $(".fondo").fadeIn(5000);
               $("#Tituloh4").bind("click",mostrarOcultarTitulo);
+              $("#enlacetecnologia").bind("click",mostrarTecnologia);
+              $("#enlacepea").bind("click",mostrarpea);
+
     }
 );
 function mostrarOcultarTitulo(){
     $("#TituloIndex").fadeToggle();
+}
+function mostrarTecnologia() {
+    $("#carreras").fadeOut();
+    $("#tecnologia").fadeIn(2000);
+}
+function mostrarpea() {
+    $("#carreras").fadeOut();
+    $("#pea").slideDown(2000);
 }
