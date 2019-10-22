377daeb69cbfdd11ce6e654812e938f189d27134
JuanColombo <juampi.colombo@hotmail.com>
2019-10-21 22:44:16 -0300

    Agregamos estilo al menu hamburguesa y mas carrera

--- js/codigo.js
+++ js/codigo.js
@@ -5,7 +5,8 @@ $(document).ready(
               $("#Tituloh4").bind("click",mostrarOcultarTitulo);
               $("#enlacetecnologia").bind("click",mostrarTecnologia);
               $("#enlacepea").bind("click",mostrarpea);
-
+              $(".volveralplan").bind("click",mostrarplandeestudio);
+              $("#enlaceeconomia").bind("click",mostrareconomia);
     }
 );
 function mostrarOcultarTitulo(){
@@ -18,4 +19,14 @@ function mostrarTecnologia() {
 function mostrarpea() {
     $("#carreras").fadeOut();
     $("#pea").slideDown(2000);
+}
+function mostrareconomia() {
+    $("#carreras").hide();
+    $("#economia").slideDown(2000);
+}
+function mostrarplandeestudio(){
+    $("#tecnologia").hide(1500);
+    $("#pea").hide(1500);
+    $("#economia").hide(1500);
+    $("#carreras").show(1500);
 }
